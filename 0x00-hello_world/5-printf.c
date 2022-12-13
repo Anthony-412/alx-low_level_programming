a849ab66bf07eee73cb4253aa1e94f4bd1902c2c
Anthony <toni.kk412@gmail.com>
2022-12-13 05:22:15 -0800

    modified file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * Main - prints with proper grammar,
+ * main - prints with proper grammar,
  * Return: Always 0
  */
 
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
