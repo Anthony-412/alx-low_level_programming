7374d656dd28e7733f4df2183ed70c2c9d4ebc46
Anthony <toni.kk412@gmail.com>
2022-12-12 20:12:11 -0800

    prit a phrase

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main prints a phase
+ * Return: 0 is exited properly
+ */
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle");
+
+   return (0);
+
+}
