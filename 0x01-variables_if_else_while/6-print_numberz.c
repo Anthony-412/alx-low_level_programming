b6962c82e0fb492d33a126f1b8824ec020c4057c
Anthony <toni.kk412@gmail.com>
2022-12-15 11:40:45 -0800

    added a file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print all single digits 
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+
+	{
+	putchar (num);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
