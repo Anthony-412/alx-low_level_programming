a447c08adddd5099e3e6d92f0f198bcf835bdbf3
Anthony <toni.kk412@gmail.com>
2022-12-15 11:55:58 -0800

    added a file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print lower alphabet in reverse order
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ka;
+
+	for (ka = 'z'; ka <= 'a'; ka++)
+	{
+		putchar (ka);
+	}
+	putchar ('\n');
+		return (0);
+}
